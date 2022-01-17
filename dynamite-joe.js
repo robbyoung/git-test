8e1e5093004baa578dbd35eee0918720b7bbc1a9
Robert Young <robertyoung10101@gmail.com>
2022-01-17 19:03:37 +1300

    Add a dynamite move

--- dynamite-joe.js
+++ dynamite-joe.js
@@ -1,12 +1,11 @@
 // This is a js port for the hit demo dynamite joe
 
-
-
 function newJoe() {
     var joe = {
         cool: 100,
         name: "Joseph",
-        run: () => { console.log("Go joe go"); }
+        run: () => { console.log("Go joe go"); },
+        dynamite: () => { console.log("Throw joe throw"); }
     };
 
     return joe;
